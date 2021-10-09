5b27df3acbf228a417f6fc7b751096514d37e956
Tim Neubert <neubert@conntac.net>
2021-10-09 13:20:48 +0200

    :sparkles: Add WelcomePage component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,60 +1,31 @@
 <template>
-  <v-app>
-    <v-app-bar
+<v-app>
+  <v-app-bar
       app
       color="primary"
       dark
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+      <v-app-bar-title>GroupFood</v-app-bar-title>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+      <v-spacer/>
 
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
-  </v-app>
+      <span>{{ username }}</span>
+  </v-app-bar>
+  <v-content>
+    <WelcomeDialog />
+  </v-content>
+</v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
+import WelcomeDialog from "./components/WelcomePage";
 export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
+  name: "App",
+  components: { WelcomeDialog },
+  data () {
+    return {
+      'username': "Username"
+    }
+  }
 };
 </script>
---
+++ frontend/src/components/WelcomePage.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-container>
+    <v-card
+        out>
+      <v-card-title>
+        Session Name
+      </v-card-title>
+      <v-card-subtitle>
+        Organized by Name
+      </v-card-subtitle>
+
+      <v-card-text>
+        <v-form>
+          <v-col>
+            <v-text-field v-model="name" label="Please enter your name" required/>
+            <v-row justify="center">
+              <v-btn
+                  color="primary"
+              >
+                Next
+              </v-btn>
+            </v-row>
+          </v-col>
+        </v-form>
+      </v-card-text>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "WelcomePage",
+  data () {
+    return {
+      "name": ""
+    };
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
